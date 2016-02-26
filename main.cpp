cef2c627393da9e66a05a04928457915aadc86cb
Maxim Verreault <verreault.maxim@gmail.com>
2016-02-26 00:04:53 -0500

    Basic sfml windows test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
-
+#include <SFML/Window.hpp>
 
 //Main entry point 
 int main()
 {
+	sf::Window window(sf::VideoMode(800,600),"",sf::Style::None); //Splash screen
+
+	//Load stuff ie: settings
+	
+	
+	window.create(sf::VideoMode(800,600),"NitroTD");
+		
+
+
+
 	return 0;
 }
